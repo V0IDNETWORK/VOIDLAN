1e94e2f6f9e87e0788202c20c97cec5e1186f2a5
V0IDNETWORK <xonevoidx@gmail.com>
2026-07-14 22:59:55 +0330

    Add screenshots

--- lib/presentation/messenger/widgets/voice_message_content.dart
+++ lib/presentation/messenger/widgets/voice_message_content.dart
@@ -1,10 +1,8 @@
 import 'dart:io';
 import 'dart:math';
-
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
-
 import '../../../core/constants/app_constants.dart';
 import '../../../data/models/chat_message_model.dart';
 import 'waveform_bars.dart';
